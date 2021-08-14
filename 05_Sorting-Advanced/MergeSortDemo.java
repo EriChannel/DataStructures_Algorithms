dead8fd6c796dcf67ca81cd4f3a904bb15bc3f91
Lập trình cùng Eri <erichannel96@gmail.com>
2021-08-14 10:22:41 +0700

    Sort

--- 05_Sorting-Advanced/MergeSortDemo.java
+++ 05_Sorting-Advanced/MergeSortDemo.java
@@ -1,73 +1,76 @@
 import java.util.Arrays;
 
-public class MergeSortDemo {
+public class MergeSortDemo{
+    public static void main(String[] args) {
+        int nums[] = {25,30,45,6,11,90,15};
+        MergeSortDemo mDemo = new MergeSortDemo();
+        // mDemo.sortArray(nums);
+         System.out.println(Arrays.toString(mDemo.sortArray(nums)));
 
-    public int[] merge(int []nums1, int []nums2){
-        int n = nums1.length + nums2.length;
-        int result[] = new int[n];
+        // int nums1[] = {6,25,30,45};
+        // int nums2[] = {11,15,90};
+        // int result[] = mDemo.merge(nums1, nums2);
+        // System.out.println(Arrays.toString(result));
+        
+    }
 
-        int i = 0;
-        int i1 = 0;
-        int i2 = 0;
-        while (i < n) {
-            if(i1 < nums1.length && i2 < nums2.length){ //nums1 và nums2 khác rỗng
-                if(nums1[i1] <= nums2[i2]){
-                    result[i] = nums1[i1];
-                    i++;
-                    i1++;
-                }else{
-                    result[i] = nums2[i2];
-                    i++;
-                    i2++;
-                }
-            }else{  //nums1 rỗng hoặc nums2 rỗng
-                if(i1 < nums1.length){  //nums1 không rỗng, nums2 rỗng
-                    result[i] = nums1[i1];
-                    i++;
-                    i1++;
-                }else{ //nums1 rỗng, nums2 không rỗng
-                    result[i] = nums2[i2];
-                    i++;
-                    i2++;
-                }
 
-            }
-        }
-
-        return result;
-
+    public int[] sortArray(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
     }
 
     public int[] mergeSort(int nums[], int iLeft, int iRight){
-        
-        //Điều kiện dừng
-        if(iLeft > iRight){
-            return new int[0]; //Trả về mảng rỗng
-        }
 
-        if(iLeft == iRight){ //Khi mảng chỉ có một phần tử
-            int []singleElement = {nums[iLeft]};
+        //Điều kiện dừng
+        if(iLeft == iRight){ //Mảng chỉ một phần tử
+            int singleElement[] = {nums[iLeft]};
             return singleElement;
         }
 
         //Chia mảng
         int iMid = (iLeft + iRight)/2;
         int nums1[] = mergeSort(nums, iLeft, iMid);
-        int nums2[] = mergeSort(nums, iMid+1, iRight);
+        int nums2[] = mergeSort(nums, iMid + 1, iRight);
 
         //Gộp mảng
         int result[] = merge(nums1, nums2);
         return result;
-        
-    }
-    public int[] sortArray(int[] nums) {
-        int n = nums.length;
-        return mergeSort(nums, 0, n-1);
+
     }
 
-    public static void main(String[] args) {
-        int nums[] = { 25, 30, 45, 6, 11, 90, 15 };
-        MergeSortDemo s = new MergeSortDemo();
-        System.out.println(Arrays.toString(s.sortArray(nums)));
+    //Gộp mảng
+    public int[] merge(int nums1[], int nums2[]){
+        int n = nums1.length + nums2.length;
+        int result[] = new int[n];
+
+        int i = 0;
+        int i1 = 0;
+        int i2 = 0;
+
+        while (i < n) {
+            if(i1 < nums1.length && i2 < nums2.length){ //nums1 và nums2 khác rỗng
+                if(nums1[i1] <= nums2[i2]){
+                    result[i] = nums1[i1];
+                    i++;
+                    i1++;
+                }else{ //nums1[i1] > nums2[i2]
+                    result[i] = nums2[i2];
+                    i++;
+                    i2++;
+                }
+            }else{ //nums1 hoặc nums2 khác rỗng
+                if(i1 < nums1.length){ //nums1 khác rỗng, nums2 rỗng
+                    result[i] = nums1[i1];
+                    i++;
+                    i1++;
+                }else{  //nums1 rỗng, nums2 khác rỗng
+                    result[i] = nums2[i2];
+                    i++;
+                    i2++;
+                }
+            }
+        }
+
+        return result;
     }
 }
--- 05_Sorting-Advanced/QuickSortDemo.java
+++ 05_Sorting-Advanced/QuickSortDemo.java
@@ -1,35 +1,37 @@
 import java.util.Arrays;
 
 public class QuickSortDemo {
-
     public static void main(String[] args) {
         QuickSortDemo qDemo = new QuickSortDemo();
-        int nums[] = { 25, 30, 45, 6, 11, 90, 15 };
+        int nums[] = {25,30,45,6,11,90,15};
         qDemo.sortArray(nums, 0, nums.length-1);
         System.out.println(Arrays.toString(nums));
     }
-    public void sortArray(int nums[], int iLeft, int iRight){
+
+    public void sortArray(int nums[], int iLeft, int iRight ){
 
         //Điều kiện dừng
         if(iLeft >= iRight){
             return;
         }
-        //Chọn khoá, ưu tiên chọn phần tử ở trung tâm
-        int iMid = (iLeft+iRight)/2;
+
+
+        //Chọn khoá
+        int iMid = (iLeft + iRight)/2;
         int key = nums[iMid];
 
 
-        //Phân bố lại mảng theo khoá key, lấy giá trị chốt
+        //Phân bố lại mảng theo key, lấy giá trị chốt pivot
         int pivot = partition(nums, iLeft, iRight, key);
+        System.out.println("Chốt: " +pivot);
 
         //Chia đôi mảng sử dụng đệ quy
         sortArray(nums, iLeft, pivot - 1);
         sortArray(nums, pivot, iRight);
-
     }
 
-    // returrn pivot
     private int partition(int[] nums, int iLeft, int iRight, int key) {
+
         int iL = iLeft;
         int iR = iRight;
 
@@ -37,20 +39,20 @@ public class QuickSortDemo {
             while(nums[iL] < key){
                 iL++;
             }
-
             while(nums[iR] > key){
                 iR--;
             }
 
-            //Đổi chỗ hai phần tử không đúng vị trí
+            //Đổi chỗ hai phần tử khôn đúng vị trí
             if(iL <= iR){
+                System.out.println("Swap " + iL +" & " +iR);
                 int temp = nums[iL];
                 nums[iL] = nums[iR];
                 nums[iR] = temp;
                 iL++;
                 iR--;
             }
-
+            System.out.println(Arrays.toString(nums));
 
         }
         return iL;
